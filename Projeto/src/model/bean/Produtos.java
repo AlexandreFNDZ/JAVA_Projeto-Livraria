1f2a0a2bfa8e066f4626e2eabddb94cf9111676c
Alexandre Fernandes <alexandre_201@hotmail.com>
2019-08-23 22:32:38 -0300

    Atualizado classes Produtos e Conexao. Criado classe ProdutosDAO

--- Projeto/src/model/bean/Produtos.java
+++ Projeto/src/model/bean/Produtos.java
@@ -7,8 +7,9 @@ package model.bean;
  */
 public class Produtos {
     private int cod_prod;
+    private String nome_prod;
     private String descrição;
-    private float unidade;
+    private String unidade;
     private float precoUni;
 
     public int getCod_prod() {
@@ -17,22 +18,33 @@ public class Produtos {
     public void setCod_prod(int cod_prod) {
         this.cod_prod = cod_prod;
     }
+    
+    public String getNome_prod() {
+        return nome_prod;
+    }
+    public void setNome_prod(String nome_prod) {
+        this.nome_prod = nome_prod;
+    }
+    
     public String getDescrição() {
         return descrição;
     }
     public void setDescrição(String descrição) {
         this.descrição = descrição;
     }
-    public float getUnidade() {
+    
+    public String getUnidade() {
         return unidade;
     }
-    public void setUnidade(float unidade) {
+    public void setUnidade(String unidade) {
         this.unidade = unidade;
     }
+    
     public float getPrecoUni() {
         return precoUni;
     }
     public void setPrecoUni(float precoUni) {
         this.precoUni = precoUni;
     }
+    
 }
---
+++ Projeto/src/model/dao/ProdutosDAO.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao;
+
+import com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException;
+import connection.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import model.bean.Produtos;
+
+/**
+ *
+ * @author Alexandre / Elias / Elzio
+ */
+public class ProdutosDAO {
+    
+    Connection con = null;
+    
+    public boolean inserirProduto(Produtos prod) throws SQLException {
+        
+        boolean inseriu = false;
+        
+        try {
+            con = new Conexao().getConnection();
+            String sql = "INSERT INTO produto (cod_produto, nome_produto, descricao, unidade, preco) values (?,?,?,?,?)";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, prod.getCod_prod());
+            stmt.setString(2, prod.getNome_prod());
+            stmt.setString(3, prod.getDescrição());
+            stmt.setString(4, prod.getUnidade());
+            stmt.setFloat(5,prod.getPrecoUni());
+            stmt.execute();
+            stmt.close();
+            inseriu = true;
+        } catch (MySQLIntegrityConstraintViolationException ex) {
+            JOptionPane.showMessageDialog(null, "Produto já Cadastrado!");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            con.close();
+        }
+        
+        return inseriu;
+    }
+    
+    public boolean excluirProduto(Produtos prod) throws SQLException {
+        
+        boolean excluiu = false;
+        
+        try {
+            con = new Conexao().getConnection();
+            String sql = "DELETE FROM produto WHERE cod_produto = ?";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, prod.getCod_prod());
+            stmt.execute();
+            stmt.close();
+            excluiu = true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            con.close();
+        }
+        
+        return excluiu;
+    }
+    
+    public ArrayList<Produtos> buscarProduto() throws SQLException {
+        ResultSet rs = null;
+        ArrayList<Produtos> listaProd = new ArrayList<Produtos>();
+        
+        try {
+            con = new Conexao().getConnection();
+            String sql = "SELECT * FROM produto";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            
+            rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                Produtos prod = new Produtos();
+                prod.setCod_prod(rs.getInt("cod_produto"));
+                prod.setNome_prod(rs.getString("nome_produto"));
+                prod.setDescrição(rs.getString("descricao"));
+                prod.setUnidade(rs.getString("unidade"));
+                prod.setPrecoUni(rs.getFloat("preco"));
+       
+                listaProd.add(prod);
+            }
+            
+            stmt.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            con.close();
+        }
+        
+        return listaProd;
+    }
+    
+    public boolean atualizarProduto(Produtos prod) throws SQLException {
+        
+        boolean atualizou = false;
+        
+        try {
+            con = new Conexao().getConnection();
+            String sql = "UPDATE produto SET nome_produto = ?,descricao = ?,unidade = ?,preco = ? WHERE cod_produto = " + prod.getCod_prod();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, prod.getNome_prod());
+            stmt.setString(2, prod.getDescrição());
+            stmt.setString(3, prod.getUnidade());
+            stmt.setFloat(4,prod.getPrecoUni());
+            stmt.execute();
+            stmt.close();
+            atualizou = true;
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            con.close();
+        }
+        
+        return atualizou;
+    }
+}
