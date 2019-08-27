494de3f60a1946631f14b865a1f63de1055170f2
Elias Pedro <Eliaspedro.8888@gmail.com>
2019-08-27 20:50:39 -0300

    ADD consulta Cliente

--- Projeto/src/view/Eireli.java
+++ Projeto/src/view/Eireli.java
@@ -57,6 +57,11 @@ public class Eireli extends javax.swing.JFrame {
         jMenu3.add(jMenuItem4);
 
         jMenuItem5.setText("Consulta");
+        jMenuItem5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem5ActionPerformed(evt);
+            }
+        });
         jMenu3.add(jMenuItem5);
 
         jMenu1.add(jMenu3);
@@ -89,6 +94,10 @@ public class Eireli extends javax.swing.JFrame {
     private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
         new Cad_Cli().setVisible(true);
     }//GEN-LAST:event_jMenuItem4ActionPerformed
+
+    private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
+        new ConsultaCliente().setVisible(true);
+    }//GEN-LAST:event_jMenuItem5ActionPerformed
 
     /**
      * @param args the command line arguments
